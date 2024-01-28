f148d0f3050e9630c3e15fb2f89abd2719d6f178
DirtDover <benjaminmbureau@gmail.com>
2024-01-28 14:12:58 +0100

    Corrigé

--- 4.Abbreviation/script.js
+++ 4.Abbreviation/script.js
@@ -1,6 +1,9 @@
+// Corigé
+
 function abreviation(str) {
-  
-}
+    const splitNames = str.split(" ");                          // retourne un tableau avec tous les éléments ou la séparation a été faite la ou il y avait un espace
+    return `${splitNames[0]} ${splitNames[1].charAt(0)}.`           // récupère le premier élément splitNames[0] + le second élément coupé après la première lettre (charAt(0))
+    }         
 
 
 /* ÉNONCÉ 📚 */
