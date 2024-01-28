0f3549d8d17d6e15b4952ebef4e819e7bbca592a
DirtDover <benjaminmbureau@gmail.com>
2024-01-28 15:31:21 +0100

    Terminé et corrigé

--- 5.SquareItUp/script.js
+++ 5.SquareItUp/script.js
@@ -1,7 +1,30 @@
+// ma version + chatgpt
+
 function squareDigits(num){
 
-}
+  let numStr = num.toString()
+  let result = ""
 
+    for (let i =0; i< numStr.length; i++) {
+      let digit = parseInt(numStr[i], 10);
+      let squaredDigit = Math.pow(digit, 2)
+      result += squaredDigit;
+    }
+    return parseInt(result, 10);
+
+  }
+
+  // Correction 
+
+  function squareDigits(num){
+
+    return Number(('' + num).split("").map(val => val *val).join(""))       // la première parenthèse transforme en string, split créé un tableau map va itéré sur chaque élément du tableau et join("") va tout recoller en string. Le number au debut permet d'avoir a la fin un nombre. et donc transformer le string de join en nombre
+  
+    }
+
+    // autre méthode 
+
+    const squaredDigits = num => +num.toString().split("").map(val => val *val).join("") 
 
 /* ÉNONCÉ 📚 */
 
