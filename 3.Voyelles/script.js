761074147da9a3b2e0e6591a30438437c0940077
DirtDover <benjaminmbureau@gmail.com>
2024-01-28 13:58:19 +0100

    Terminé et corrigé

--- 3.Voyelles/script.js
+++ 3.Voyelles/script.js
@@ -1,7 +1,35 @@
-function getVowelsNumber(txt) {
+// Ma version + ChatGpt
 
+function getVowelsNumber(txt) {
+    let count = 0
+    
+
+    for( let i = 0; i < txt.length; i++){
+        if(/[aeiouy]/.test(txt[i])) {
+            count++
+        }   
+    }
+    
+    return count;   
 }
 
+// Corection
+
+function getVowelsNumber(txt) {
+    const vowels = txt.match(/[aeiouy]/gi)                      //le g veut dire gloabl ( ca ne s'arrète pas a la première reussite et i veut dire non sensible à la casse (les espaces))
+    
+    if(vowels){
+        return vowels.length
+    }
+    else {
+        return 0;
+    }
+   
+}
+
+// autre possibilité 
+
+const getVowelsNumber = txt => txt.match(/[aeiouy]/gi) ? txt.match(/[aeiouy]/gi).length : 0 
 
 /* ÉNONCÉ 📚 */
 
