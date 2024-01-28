4ca25c80c1fba0d0ff33c7505d4488c70a5bdaaf
DirtDover <benjaminmbureau@gmail.com>
2024-01-28 15:53:40 +0100

    Corrigé

--- 6.Factorial/script.js
+++ 6.Factorial/script.js
@@ -1,5 +1,12 @@
-function factorial(num) {
+// corrigé
 
+function factorial(num) {
+    if(num === 0 || num === 1) return 1;
+
+    for(let i = num - 1; i>= 1; i-- ){
+        num = num * i
+    }
+    return num
 }
 
 /* ÉNONCÉ 📚 */
