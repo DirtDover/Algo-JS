f2c395f7f7129decb12e6664461a19d2a98cb0d0
DirtDover <benjaminmbureau@gmail.com>
2024-01-29 20:16:44 +0100

    Terminé et corrigé

--- 17.getLongestWord/script.js
+++ 17.getLongestWord/script.js
@@ -1,7 +1,33 @@
+// Ma version qui fonctionne
+
 function getLongestWord(str) {
 
+        let number = []
+        let words = str.split(" ")
+    
+        words.map(el => {
+            number.push(el.length)
+        })
+         return Math.max.apply(null, number)
+        
 }
 
+// correction
+
+function getLongestWord(str){
+    const arrayFromStr = str.split(" ")
+    let longestLength = 0;
+
+    for(let i = 0; i < arrayFromStr; i++){
+        if(arrayFromStr[i].length > longestLength) {
+            longestLength = arrayFromStr[i].length;
+        }
+    }
+    return longestLength;
+}
+// Autre méthode
+
+const longestWord = str => str.split(" ").reduce((acc, cur) => Math.max(acc, cur.length), 0)
 
 /* ÉNONCÉ 📚 */
 
